395ce8a2fcc0f1cf5055d8a21bad59a12d412e87
impactCn <550569627@qq.com>
2019-02-21 15:20:38 +0800

    优化Simple14 2019.02.21

--- src/Simple14.java
+++ src/Simple14.java
@@ -52,29 +52,56 @@ public class Simple14 {
 //        }
 //
 //        return place;
+        // 时间消耗过长
+//        int lowerBound = 0;
+//        int upperBound = nums.length - 1;
+//        int middle;
+//
+//        while (true) {
+//            middle = (lowerBound + upperBound) / 2;
+//            if (nums[middle] == target) {
+//                for (int i = 0; i <= middle; i++) {
+//                    if (nums[i] == target) {
+//                        return i;
+//                    }
+//                }
+//            } else if (lowerBound > upperBound) {
+//                return -1;
+//            } else {
+//                if (nums[middle] < target) {
+//                    lowerBound = middle + 1;
+//                } else {
+//                    upperBound = middle - 1;
+//                }
+//            }
+//        }
 
-        int lowerBound = 0;
-        int upperBound = nums.length - 1;
-        int middle;
+        // 优化二分法
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
 
-        while (true) {
-            middle = (lowerBound + upperBound) / 2;
-            if (nums[middle] == target) {
-                for (int i = 0; i <= middle; i++) {
-                    if (nums[i] == target) {
-                        return i;
-                    }
-                }
-            } else if (lowerBound > upperBound) {
-                return -1;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+                // or start = mid + 1
             } else {
-                if (nums[middle] < target) {
-                    lowerBound = middle + 1;
-                } else {
-                    upperBound = middle - 1;
-                }
+                end = mid;
+                // or end = mid - 1
             }
         }
+
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        return -1;
 
 
     }
