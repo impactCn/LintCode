56f67a160d12d823d45b48aecf72cd2eb28137ec
impactCn <550569627@qq.com>
2019-02-21 16:03:31 +0800

    优化Simple39 2019.02.21

--- src/Simple39.java
+++ src/Simple39.java
@@ -14,41 +14,31 @@ public class Simple39 {
      * @param nums: An integer array
      * @return: nothing
      */
-    public void recoverRotatedSortedArray(List<Integer> nums) {
+    public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
         // write your code here
-        // 桶排序
-
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < nums.size(); i++) {
-            max = Math.max(max, nums.get(i));
-            min = Math.min(min, nums.get(i));
+        for (int i = 0; i < nums.size() - 1; i++) {
+            // 寻找第一个比后面大的数
+            // 以[4, 5, 1, 2, 3]为例
+            // 找到5，翻转[4, 5]得到[5, 4]
+            // 翻转[1, 2, 3]得到[3, 2, 1]
+            // 最后翻转[5, 4, 3, 2, 1]得到[1, 2, 3, 4, 5]
+            if (nums.get(i) > nums.get(i + 1)) {
+                reverse(nums, 0, i);
+                reverse(nums, i + 1, nums.size() - 1);
+                reverse(nums, 0, nums.size() - 1);
+                return;
+            }
         }
+    }
 
-        // 桶数
-        int bucketNum = (max - min) / nums.size() + 1;
-        int[] bucketArr = new int[bucketNum];
-
-        for (int i = 0; i < nums.size(); i++) {
-            int mapPos = nums.get(i) - min;
-            bucketArr[mapPos]++;
+    private void reverse(ArrayList<Integer> nums, int start, int end) {
+        // 冒泡排序
+        // 这里可以选择更优的排序方法
+        for (int i = start, j = end; i < j; i++, j--) {
+            int temp = nums.get(i);
+            nums.set(i, nums.get(j));
+            nums.set(j, temp);
         }
-
-        for (int i = 0; i < bucketArr.length; i++) {
-            bucketArr[i] = bucketArr[i - 1] + bucketArr[i];
-        }
-
-        int res[] = new int[nums.size()];
-
-        for (int i = 0; i < nums.size(); i++) {
-            int post = --bucketArr[nums.get(i) - min];
-            res[post] = nums.get(i);
-        }
-
-
-
     }
     
 }
